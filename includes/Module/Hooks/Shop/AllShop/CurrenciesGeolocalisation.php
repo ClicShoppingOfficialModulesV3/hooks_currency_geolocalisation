c77a6656623115b54f4bc7fe3bd3087e4cda7c0a
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2019-12-30 14:56:11 -0500

    version3.2113

--- includes/Module/Hooks/Shop/AllShop/CurrenciesGeolocalisation.php
+++ includes/Module/Hooks/Shop/AllShop/CurrenciesGeolocalisation.php
@@ -40,7 +40,7 @@
      * @param $localisation_array return an array on the localisation
      * @access public
     */
-    private function setUrlAPI()
+    private function setUrlAPI(): string
     {
 
       if (!empty($this->SSLKey) && !is_null($this->SSLKey)) {
@@ -48,6 +48,7 @@
       } else {
         $url = $this->UrlAPI . $this->ipCustomer;
       }
+
       return $url;
     }
 
@@ -60,9 +61,10 @@
      * @return $repo,values of array of all git information
      * @access public
      */
-    public function displayDataAPI()
+    public function displayDataAPI(): string
     {
       $data = '<pre>' . print_r($this->setUrlAPI(), true) . '</pre>';
+
       return $data;
     }
 
@@ -72,7 +74,7 @@
      * @return $result all data for customer identification and insert in cahce
      * @access private
      */
-    private function getJsonCustomerData()
+    private function getJsonCustomerData(): string
     {
 
       $ip = str_replace('.', '_', $this->ipCustomer);
@@ -114,7 +116,7 @@
      * @return $country_code,code iso 2 of the country - FR
      * @access public
      */
-    public function getCustomerCountryCode()
+    public function getCustomerCountryCode(): string
     {
 
       $data = $this->getJsonCustomerData();
@@ -129,7 +131,7 @@
      * @return $country_code, name of the country - FRANCE
      * @access public
      */
-    public function getCustomerCountryName()
+    public function getCustomerCountryName(): string
     {
 
       $data = $this->getJsonCustomerData();
@@ -144,7 +146,7 @@
      * @return $region_code, region of the country name - Jura
      * @access public
      */
-    public function getCustomerRegionCode()
+    public function getCustomerRegionCode(): string
     {
 
       $data = $this->getJsonCustomerData();
@@ -159,7 +161,7 @@
      * @return $region, baem of region of the country name - Jura
      * @access public
      */
-    public function getCustomerRegionName()
+    public function getCustomerRegionName(): string
     {
 
       $data = $this->getJsonCustomerData();
@@ -175,7 +177,7 @@
      * @access public
      */
 
-    public function getCustomerContinent()
+    public function getCustomerContinent(): string
     {
 
       $data = $this->getJsonCustomerData();
@@ -190,9 +192,8 @@
      * @access public
      * osc_get_currencies_location
     */
-    public function GetCurrenciesLocation()
+    public function GetCurrenciesLocation(): string
     {
-
       $country_code2 = $this->getCustomerCountryCode();
 
       if ($this->getCustomerContinent() == 'NA') {
@@ -255,57 +256,30 @@
     private function install()
     {
       $CLICSHOPPING_Db = Registry::get('Db');
-      $CLICSHOPPING_Language = Registry::get('Language');
 
-      if ($CLICSHOPPING_Language->getId() == 1) {
-        $CLICSHOPPING_Db->save('configuration', [
-            'configuration_title' => 'Souhaitez-vous afficher une devise automatique par défaut en fonction du continent ?',
-            'configuration_key' => 'CONFIGURATION_CURRENCIES_GEOLOCALISATION',
-            'configuration_value' => 'false',
-            'configuration_description' => 'En fonction de la provenance du client et de son continent, le tarif du produit prend par défaut la devise du continent du client.<br /><br /><u><strong>Note :</strong></u><br />- Les devises par défaut implémentées sont : USD, EUR, CAD <br />- Si le client provient d\'un autre continent, ce sera la devise par défaut qui s\'affichera.<br />- L\'ajustement automatique des devises en fonction de la langue ne fonctionne pas dans ce cas.<br />L\'analyse de l\'addresseIP du client est faite à partir de ce site : http://geoplugin.net.<br />Veuillez lire leurs instructions.<br /><br /><i>(Valeur True = Oui - Valeur False = Non)</i>',
-            'configuration_group_id' => '1',
-            'sort_order' => '9',
-            'set_function' => 'clic_cfg_set_boolean_value(array(\'true\', \'false\'))',
-            'date_added' => 'now()'
-          ]
-        );
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Do you want to display an automatic default currency by continent ?',
+          'configuration_key' => 'CONFIGURATION_CURRENCIES_GEOLOCALISATION',
+          'configuration_value' => 'false',
+          'configuration_description' => 'Depending on the source of the client and the continent, the price is updated in function the customer continent.<br /><br /><u><strong>Note :</strong></u><br />- The default currency implemented are: USD, EUR, CAD <br />- If the customer comes from another continent, it will be the default currency that is displayed.<br />- The automatic adjustment of currencies depending the language does not work in this case.',
+          'configuration_group_id' => '1',
+          'sort_order' => '9',
+          'set_function' => 'clic_cfg_set_boolean_value(array(\'true\', \'false\'))',
+          'date_added' => 'now()'
+        ]
+      );
 
-        $CLICSHOPPING_Db->save('configuration', [
-            'configuration_title' => 'Souhaitez-vous indiquer la clef pour une certification SSL de géolocalisation en fonction de la devise automatique continent (non obligatoire) ?',
-            'configuration_key' => 'CONFIGURATION_CURRENCIES_GEOLOCALISATION_SSLKEY',
-            'configuration_value' => '',
-            'configuration_description' => 'Le site http://geoplugin.net peut vous fournir une clef qui vous permettra d\avoir une connexion sécurisée. Veuillez vous y référer pour plus d\'informations</i>',
-            'configuration_group_id' => '1',
-            'sort_order' => '9',
-            'set_function' => '',
-            'date_added' => 'now()'
-          ]
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Do you want insert a Key for SSL certification for your automatic currencies by continent (no mandatory) ?',
+          'configuration_key' => 'CONFIGURATION_CURRENCIES_GEOLOCALISATION_SSLKEY',
+          'configuration_value' => '',
+          'configuration_description' => 'The website http://geoplugin.net can propose you a SSL key for a secure connexion. Please go on website and see their documentation',
+          'configuration_group_id' => '1',
+          'sort_order' => '9',
+          'set_function' => '',
+          'date_added' => 'now()'
+        ]
         );
-      } else {
-        $CLICSHOPPING_Db->save('configuration', [
-            'configuration_title' => 'Do you want to display an automatic default currency by continent ?',
-            'configuration_key' => 'CONFIGURATION_CURRENCIES_GEOLOCALISATION',
-            'configuration_value' => 'false',
-            'configuration_description' => 'Depending on the source of the client and the continent, the price is updated in function the customer continent.<br /><br /><u><strong>Note :</strong></u><br />- The default currency implemented are: USD, EUR, CAD <br />- If the customer comes from another continent, it will be the default currency that is displayed.<br />- The automatic adjustment of currencies depending the language does not work in this case.',
-            'configuration_group_id' => '1',
-            'sort_order' => '9',
-            'set_function' => 'clic_cfg_set_boolean_value(array(\'true\', \'false\'))',
-            'date_added' => 'now()'
-          ]
-        );
-
-        $CLICSHOPPING_Db->save('configuration', [
-            'configuration_title' => 'Do you want insert a Key for SSL certification for your automatic currencies by continent (no mandatory) ?',
-            'configuration_key' => 'CONFIGURATION_CURRENCIES_GEOLOCALISATION_SSLKEY',
-            'configuration_value' => '',
-            'configuration_description' => 'The website http://geoplugin.net can propose you a SSL key for a secure connexion. Please go on website and see their documentation',
-            'configuration_group_id' => '1',
-            'sort_order' => '9',
-            'set_function' => '',
-            'date_added' => 'now()'
-          ]
-        );
-      }
     }
 
     public function execute()
@@ -314,6 +288,7 @@
         $this->install();
 
         Cache::clear('menu-administrator');
+        Cache::clear('configuration');
       }
 
       if ($this->spiderFlag === false && defined(CONFIGURATION_CURRENCIES_GEOLOCALISATION)) {
